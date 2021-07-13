f51095652601379aa2eeff320714658f7878cf95
omprakash_parvendhan <ompargeetha@gmail.com>
2021-07-13 19:00:15 +0530

    Code changes

--- src/main/java/com/ash/programming/application/OrganisationApplication.java
+++ src/main/java/com/ash/programming/application/OrganisationApplication.java
@@ -1,22 +1,42 @@
 package com.ash.programming.application;
 
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 
 @SpringBootApplication(scanBasePackages = {"com"})
 @MapperScan("com.ash.programming.dao.mapper")
 public class OrganisationApplication  extends SpringBootServletInitializer{
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(OrganisationApplication.class);
-    }
+	
+	private String JNDI_DS = "java:jboss/organisationDS";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		SpringApplication.run(OrganisationApplication.class, args);
 	}
-
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder) {
+        return springApplicationBuilder.sources(OrganisationApplication.class);
+    }
+	
+	@Bean
+	public DataSource dataSource() throws IllegalArgumentException, NamingException {
+		JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+		jndiObjectFactoryBean.setJndiName(JNDI_DS);
+		jndiObjectFactoryBean.setProxyInterface(DataSource.class);
+		jndiObjectFactoryBean.setResourceRef(true);
+		jndiObjectFactoryBean.afterPropertiesSet();
+		return (DataSource)jndiObjectFactoryBean.getObject();
+		
+	}
+	
 }
--- src/main/java/com/ash/programming/dao/mapper/OrganisationMapper.java
+++ src/main/java/com/ash/programming/dao/mapper/OrganisationMapper.java
@@ -2,11 +2,11 @@ package com.ash.programming.dao.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
-
 import com.ash.programming.dao.vo.Employee;
 import com.ash.programming.dao.vo.Structure;
 
@@ -29,7 +29,10 @@ public interface OrganisationMapper {
 		@Result(column="COUNTRY", property="address.country"),
 		@Result(column="PINCODE", property="address.pincode"),
 		@Result(column="PASSPORT_NO", property="passportNumber"),
-		@Result(column="PRIMARY_SKILL", property="primarySkill")
+		@Result(column="PRIMARY_SKILL", property="primarySkill"),
+		@Result(column="PAST_EXPERIENCE", property="pastExperience"),
+		@Result(column="EXPERIENCE", property="compExperience"),
+		@Result(column="SALARY", property="salary")
 	})
 	@Select("select * from EMP_PROFILE")
 	public List<Employee> getEmployees();
@@ -59,10 +62,17 @@ public interface OrganisationMapper {
 		@Result(column="COUNTRY", property="address.country"),
 		@Result(column="PINCODE", property="address.pincode"),
 		@Result(column="PASSPORT_NO", property="passportNumber"),
-		@Result(column="PRIMARY_SKILL", property="primarySkill")
+		@Result(column="PRIMARY_SKILL", property="primarySkill"),
+		@Result(column="PAST_EXPERIENCE", property="pastExperience"),
+		@Result(column="EXPERIENCE", property="compExperience"),
+		@Result(column="SALARY", property="salary")
 	})
 	@Select("SELECT * FROM EMP_PROFILE WHERE EMP_ID = #{empID}")
 	public Employee getEmployee(Integer empID);
 	
-	
+	@Insert("INSERT INTO EMP_PROFILE (EMP_ID, EMP_NAME, EMP_DEPARTMENT, EMP_ROLE, DOJ, EMAIL_ID, PERSONAL_EMAIL, ADDRESS_LN1, ADDRESS_LN2, ADDRESS_LN3,"
+			+ "CITY, STATE, COUNTRY, PINCODE, PASSPORT_NO, PRIMARY_SKILL, PAST_EXPERIENCE, EXPERIENCE, SALARY) VALUES "
+			+ "(#{employeeID}, #{employeeName}, #{employeeDepartment}, #{role}, #{dateOfJoining}, #{emailId}, #{personalEmail}, #{address.addressLn1}, #{address.addressLn2}, #{address.addressLn3}, "
+			+ "#{address.city}, #{address.state}, #{address.country}, #{address.pincode}, #{passportNumber}, #{primarySkill}, #{pastExperience}, #{compExperience}, #{salary})")
+	void createEmployee(Employee employee);
 }
